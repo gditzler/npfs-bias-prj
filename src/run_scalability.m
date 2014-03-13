f8d4f42fce437eda1a69679746c1a918dea6060f
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-13 17:53:26 -0400

    add in some averageing

--- src/run_scalability.m
+++ src/run_scalability.m
@@ -18,9 +18,10 @@ n_relevant = 10;
 n_features = [50,100:50:500,750,1000,2000];
 n_boots = [100,250];
 n_select = [5,10,25,50];
-n_samples = 1000;
+n_samples = 5000;
 method = 'mim';
 alpha = 0.01;
+avg = 20;
 
 matlabpool close force
 matlabpool open local 12
@@ -29,18 +30,25 @@ recalls = zeros(numel(n_features),numel(n_boots),numel(n_select));
 jaccards = zeros(numel(n_features),numel(n_boots),numel(n_select));
 lustgarten = zeros(numel(n_features),numel(n_boots),numel(n_select));
 
-for nf = 1:numel(n_features)
-  for nb = 1:numel(n_boots)
-    for ns = 1:numel(n_select)
-      disp(['NF:',num2str(n_features(nf)),', NB:',num2str(n_boots(nb)),...
-        ', NS:',num2str(n_select(ns))]);
-      [data,labels] = uni_data(n_samples, n_features(nf), n_relevant, 'hard');
-      idx = npfs(data, labels, method, n_select(ns), n_boots(nb), alpha, 0);
-      recalls(nf,nb,ns) = calc_recall(1:n_relevant, idx);
-      jaccards(nf,nb,ns) = calc_jaccard(1:n_relevant, idx);
-      lustgarten(nf,nb,ns) = calc_lustgarten(1:n_relevant, idx, n_features(nf));
+for a = 1:avg
+  for nf = 1:numel(n_features)
+    for nb = 1:numel(n_boots)
+      for ns = 1:numel(n_select)
+        disp(['NF:',num2str(n_features(nf)),', NB:',num2str(n_boots(nb)),...
+          ', NS:',num2str(n_select(ns))]);
+        [data,labels] = uni_data(n_samples, n_features(nf), n_relevant, 'hard');
+        idx = npfs(data, labels, method, n_select(ns), n_boots(nb), alpha, 0);
+        recalls(nf,nb,ns) = recalls(nf,nb,ns) + calc_recall(1:n_relevant, idx);
+        jaccards(nf,nb,ns) = jaccards(nf,nb,ns) + calc_jaccard(1:n_relevant, idx);
+        lustgarten(nf,nb,ns) = lustgarten(nf,nb,ns) + calc_lustgarten(1:n_relevant, ...
+          idx, n_features(nf));
+      end
     end
   end
 end
+recalls = recalls/avg;
+jaccards = jaccards/avg;
+lustgarten = lustgarten/avg;
+
 matlabpool close force;
 save('../mat/experiment_scalability.mat');
