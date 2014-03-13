b43b28f11981d3b886a5d90650a60fa41b44bc1b
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-13 07:19:24 -0700

    fixed an error with the call to calc_lustgarten and added some verbosity

--- src/run_scalability.m
+++ src/run_scalability.m
@@ -3,6 +3,7 @@ clear;
 close all;
 
 addpath('utils/');
+addpath(genpath('~/Git/thesis-code/feat_sel/FEAST/'));
 
 n_relevant = 10;
 n_features = [50,100:50:500,750,1000,2000];
@@ -15,16 +16,22 @@ alpha = 0.01;
 matlabpool close force
 matlabpool open local 12
 
+recalls = zeros(numel(n_features),numel(n_boots),numel(n_select));
+jaccards = zeros(numel(n_features),numel(n_boots),numel(n_select));
+lustgarten = zeros(numel(n_features),numel(n_boots),numel(n_select));
 
 for nf = 1:numel(n_features)
   for nb = 1:numel(n_boots)
     for ns = 1:numel(n_select)
+      disp(['NF:',num2str(n_features(nf)),', NB:',num2str(n_boots(nb)),...
+        ', NS:',num2str(n_select(ns))]);
       [data,labels] = uni_data(n_samples, n_features(nf), n_relevant, 'hard');
-      idx = npfs(data, labels, method, n_select, n_boots(nb), alpha, 0);
+      idx = npfs(data, labels, method, n_select(ns), n_boots(nb), alpha, 0);
       recalls(nf,nb,ns) = calc_recall(1:n_relevant, idx);
       jaccards(nf,nb,ns) = calc_jaccard(1:n_relevant, idx);
-      lustgarten(nf,nb,ns) = calc_lustgarten(1:n_relevant, idx);
+      lustgarten(nf,nb,ns) = calc_lustgarten(1:n_relevant, idx, n_features(nf));
     end
   end
 end
+matlabpool close force;
 save('../mat/experiment_scalability.mat');
