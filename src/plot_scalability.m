a3d08b0d2d0f72d2fe20567b2ff2c1cb2831f387
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-14 14:04:02 -0400

    updated the plots scripts

--- src/plot_scalability.m
+++ src/plot_scalability.m
@@ -2,7 +2,7 @@ clc
 clear
 close all
 
-load ../mat/experiment_scalability.mat
+load ../mat/experiment_scalability_mim.mat
 
 c = {'k-^','b-*','r-p','c-s','g-o','co','k--'};
 fs = 26;
@@ -35,6 +35,17 @@ hold on;
 box on;
 for i = 1:size(recalls,3)
   plot(n_features, recalls(:,boot_idx,i), c{i}, 'LineWidth',2)
+end
+legend('k=5','k=10','k=25','k=50','Location','best')
+xlabel('# of features','FontSize',fs)
+ylabel('Recall','FontSize',fs)
+set(gca,'fontsize',fs)
+
+figure;
+hold on;
+box on;
+for i = 1:size(recalls,3)
+  plot(n_features, selection_size(:,boot_idx,i), c{i}, 'LineWidth',2)
 end
 legend('k=5','k=10','k=25','k=50','Location','best')
 xlabel('# of features','FontSize',fs)
